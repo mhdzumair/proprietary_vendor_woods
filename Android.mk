1db50b96ad6954458f43e902437c30574bfe6ff8
mhdzumair <mhdzumair@gmail.com>
2020-04-27 17:09:32 +0000

    blobs:point out prebuilt libs
    Signed-off-by: mhdzumair <mhdzumair@gmail.com>

--- Android.mk
+++ Android.mk
@@ -35,14 +35,6 @@ LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libmtkcam_fwkutils.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE = libion_mtk
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
